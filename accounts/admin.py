2ab534b74142a7d04ecee58527c5fbe1182449ed
Nazarov Behruzbek <78092336+NazarovBehruzbek@users.noreply.github.com>
2025-09-22 17:08:43 +0500

    register User model in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("id", "username", "is_active", "is_staff", "is_superuser")
+    search_fields = ("username",)
+    ordering = ("id",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
